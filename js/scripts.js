9a1973ffe85621f25fac0cee4b301a4109df55b2
Michal Kizling <mkizling@gmail.com>
2017-06-11 21:44:49 +0200

    Basic project files

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var textInButton = document.getElementsByClassName('button');
+
+for (var i = 0 ; i < textInButton.length ; i++) {
+	alert(textInButton[i].textContent);
+	console.log(textInButton[i].textContent);
+}
